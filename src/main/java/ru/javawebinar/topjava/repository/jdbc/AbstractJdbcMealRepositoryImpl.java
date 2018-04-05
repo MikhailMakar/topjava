98f536d0baa5b72d7ee596a5b89088151b802260
Mikhail Makar <makar.mikhail@yandex.ru>
2018-04-05 13:01:46 +0400

    Fixed realisation for hsqldb and postgres because of LocalDateTime

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import ru.javawebinar.topjava.model.Meal;
@@ -35,7 +36,28 @@ public abstract class AbstractJdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public abstract Meal save(Meal meal, int userId);
+    public Meal save(Meal meal, int userId){
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("date_time", getFixedDate(meal.getDateTime()))
+                .addValue("user_id", userId);
+
+        if (meal.isNew()) {
+            Number newId = insertMeal.executeAndReturnKey(map);
+            meal.setId(newId.intValue());
+        } else {
+            if (namedParameterJdbcTemplate.update("" +
+                            "UPDATE meals " +
+                            "   SET description=:description, calories=:calories, date_time=:date_time " +
+                            " WHERE id=:id AND user_id=:user_id"
+                    , map) == 0) {
+                return null;
+            }
+        }
+        return meal;
+    }
 
     @Override
     public boolean delete(int id, int userId) {
@@ -56,5 +78,11 @@ public abstract class AbstractJdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public abstract List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId){
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, getFixedDate(startDate), getFixedDate(endDate));
+    }
+
+    public abstract Object getFixedDate (LocalDateTime localDateTime);
 }
--- src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlDbJdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqlDbJdbcMealRepositoryImpl.java
@@ -20,33 +20,10 @@ public class HsqlDbJdbcMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl
     }
 
     @Override
-    public Meal save(Meal meal, int userId) {
-        MapSqlParameterSource map = new MapSqlParameterSource()
-                .addValue("id", meal.getId())
-                .addValue("description", meal.getDescription())
-                .addValue("calories", meal.getCalories())
-                .addValue("date_time", Timestamp.valueOf(meal.getDateTime()))
-                .addValue("user_id", userId);
-
-        if (meal.isNew()) {
-            Number newId = insertMeal.executeAndReturnKey(map);
-            meal.setId(newId.intValue());
-        } else {
-            if (namedParameterJdbcTemplate.update("" +
-                            "UPDATE meals " +
-                            "   SET description=:description, calories=:calories, date_time=:date_time " +
-                            " WHERE id=:id AND user_id=:user_id"
-                    , map) == 0) {
-                return null;
-            }
-        }
-        return meal;
-    }
-
-    @Override
-    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, Timestamp.valueOf(startDate), Timestamp.valueOf(endDate));
+    public Object getFixedDate(LocalDateTime localDateTime) {
+        return Timestamp.valueOf(localDateTime);
     }
 }
+
+
+
